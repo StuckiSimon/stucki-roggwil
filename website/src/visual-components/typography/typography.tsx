02073feac85efd5c9ce48b2cef03237023d787fa
Simon Stucki <simon_stucki@hotmail.com>
2025-03-12 20:18:58 +0100

    Setup reusable typography

---
+++ website/src/visual-components/typography/typography.tsx
@@ -0,0 +1,40 @@
+import React, { JSX } from 'react';
+import classNames from 'classnames';
+import styles from './typography.module.scss';
+
+export type TypographyVariant = 'title-1' | 'title-2' | 'sub-title' | 'buttontext' | 'text';
+
+export type TypographyColor = 'black' | 'blue' | 'white';
+
+const VARIANT_DEFAULT_TAG_MAP: Record<TypographyVariant, keyof JSX.IntrinsicElements> = {
+  'title-1': 'h1',
+  'title-2': 'h1',
+  'sub-title': 'h2',
+  buttontext: 'p',
+  text: 'p',
+};
+
+type Props = {
+  variant?: TypographyVariant;
+  tag?: keyof JSX.IntrinsicElements;
+  color?: TypographyColor;
+} & React.HTMLAttributes<HTMLOrSVGElement>;
+
+export const Typography: React.FC<Props> = ({ tag, variant = 'text', color = 'black', className, ...rest }) => {
+  const Component = tag === undefined ? VARIANT_DEFAULT_TAG_MAP[variant] : tag;
+  const colorClassname = `color-${color}`;
+
+  return (
+    <Component
+      className={classNames(
+        styles.typography,
+        styles[variant],
+        {
+          [styles[colorClassname]]: color !== 'black',
+        },
+        className,
+      )}
+      {...rest}
+    />
+  );
+};
