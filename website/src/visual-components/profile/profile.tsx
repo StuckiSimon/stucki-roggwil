47e4cc2b42b7b7227893f98a7bec576efc0082dc
Simon Stucki <simon_stucki@hotmail.com>
2025-03-16 07:28:37 +0100

    Leverage spacer

--- website/src/visual-components/profile/profile.tsx
+++ website/src/visual-components/profile/profile.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { notNil } from '@/core/util/is-nil';
 import styles from './profile.module.scss';
 import { Typography } from '@/visual-components/typography/typography';
+import { Spacer } from '@/visual-components/spacer/spacer';
 
 type Props = {
   title: string;
@@ -14,10 +15,14 @@ export const Profile: React.FC<Props> = ({ title, subtitle, image }) => {
   return (
     <div>
       {hasImage ? <img src={`${image}?w=600&h=450&fit=crop`} alt={title} /> : null}
+      <Spacer size="04" />
       <Typography variant="sub-title" color="blue" className={styles.title}>
         {title}
       </Typography>
-      <Typography variant="text">{subtitle}</Typography>
+      <Spacer size="02" />
+      <Typography variant="text" className={styles.text}>
+        {subtitle}
+      </Typography>
     </div>
   );
 };
