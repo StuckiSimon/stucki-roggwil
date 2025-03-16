2bd38710ff4fa8c5d5f05672a8901d53c8b2a188
Simon Stucki <simon_stucki@hotmail.com>
2025-03-16 07:28:55 +0100

    Improve sizing

--- website/src/visual-components/footer/footer.tsx
+++ website/src/visual-components/footer/footer.tsx
@@ -3,6 +3,7 @@ import logoSvg from '@/visual-components/shared-assets/logo.svg';
 import styles from './footer.module.scss';
 import { GridContainer, GridItem } from '@/visual-components/grid/grid';
 import { Typography } from '@/visual-components/typography/typography';
+import { Spacer } from '@/visual-components/spacer/spacer';
 
 export const Footer: React.FC = () => {
   return (
@@ -14,17 +15,17 @@ export const Footer: React.FC = () => {
         <GridItem span="3">
           <Typography variant="text" color="white" className={styles.addressBlock}>
             Garage Stucki AG
-            <br />
+            <Spacer size="02" />
             St. Urbanstrasse 35
-            <br />
+            <Spacer size="02" />
             4914 Roggwil (BE)
           </Typography>
         </GridItem>
         <GridItem span="4">
           <Typography variant="text" color="white" className={styles.addressBlock}>
-            062 929 05 05
-            <br />
-            info@stucki-roggwil.ch
+            <a href="tel:+410629290505">062 929 05 05</a>
+            <Spacer size="02" />
+            <a href="mailto:info@stucki-roggwil.ch">info@stucki-roggwil.ch</a>
           </Typography>
         </GridItem>
         <GridItem span="5"></GridItem>
