d91a5c968088443e6aa02b3dd124df5229db65c6
Simon Stucki <simon_stucki@hotmail.com>
2025-03-18 17:13:19 +0100

    Extend link behavior

--- website/src/visual-components/link/link.tsx
+++ website/src/visual-components/link/link.tsx
@@ -5,13 +5,24 @@ import styles from './link.module.scss';
 
 type Props = ComponentProps<typeof NextLink> & {
   accent?: boolean;
+  animated?: boolean;
+  active?: boolean;
 };
 
-export const Link: React.FC<Props> = ({ className, children, accent = false, ...rest }) => {
+export const Link: React.FC<Props> = ({
+  className,
+  children,
+  accent = false,
+  animated = false,
+  active = false,
+  ...rest
+}) => {
   return (
     <NextLink
       className={classNames(className, styles.root, {
         [styles.accent]: accent,
+        [styles.animatedUnderline]: animated,
+        [styles.active]: active,
       })}
       {...rest}
     >
