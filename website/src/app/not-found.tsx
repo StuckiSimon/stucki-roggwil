907ee264d79d23468b522a2601a8adc77a2d1805
Simon Stucki <simon_stucki@hotmail.com>
2025-03-21 08:08:01 +0100

    Add 404 page

---
+++ website/src/app/not-found.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { GridContainer, GridItem } from '@/visual-components/grid/grid';
+import { Layout } from '@/modules/layout/layout';
+import { Typography } from '@/visual-components/typography/typography';
+import { Spacer } from '@/visual-components/spacer/spacer';
+import { Link } from '@/visual-components/link/link';
+
+export const NotFoundPage: React.FC = () => {
+  return (
+    <Layout>
+      <GridContainer>
+        <GridItem>
+          <Spacer size="08" />
+          <Typography variant="title-1">404</Typography>
+          <Spacer size="03" />
+          <Typography variant="sub-title">Seite nicht gefunden</Typography>
+          <Spacer size="06" />
+          <Typography variant="text" tag="span">
+            Wie wär&#39;s mit etwas anderem:{' '}
+          </Typography>
+          <Link href="/services">Zu den Dienstleistungen</Link>
+          <Typography variant="text" tag="span">
+            {' '}
+            oder direkt{' '}
+          </Typography>
+          <Link href="/">zur Startseite</Link>
+        </GridItem>
+      </GridContainer>
+    </Layout>
+  );
+};
+
+export default NotFoundPage;
