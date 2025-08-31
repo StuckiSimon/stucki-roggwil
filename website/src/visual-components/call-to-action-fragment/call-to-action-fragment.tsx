946f11f0612ffae51221c20dd62d985766c664ae
Simon Stucki <simon_stucki@hotmail.com>
2025-08-31 13:25:30 +0200

    Add call-to-action fragment

---
+++ website/src/visual-components/call-to-action-fragment/call-to-action-fragment.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from 'react';
+import styles from './call-to-action-fragment.module.scss';
+import { Typography } from '@/visual-components/typography/typography';
+import { Spacer } from '@/visual-components/spacer/spacer';
+
+type Props = {
+  title: string;
+  text: string;
+  children: ReactNode;
+};
+
+export const CallToActionFragment: React.FC<Props> = ({ title, text, children }) => {
+  return (
+    <div className={styles.root}>
+      <div>
+        <Typography variant="sub-title" color="blue">
+          {title}
+        </Typography>
+        <Spacer size="03" />
+        <Typography variant="text">{text}</Typography>
+      </div>
+      <div>{children}</div>
+    </div>
+  );
+};
