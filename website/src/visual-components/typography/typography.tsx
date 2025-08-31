0399941b3297837491bc84d0d831fdeb3770c1c8
Simon Stucki <simon_stucki@hotmail.com>
2025-08-31 13:23:53 +0200

    Add third level of page headers

--- website/src/visual-components/typography/typography.tsx
+++ website/src/visual-components/typography/typography.tsx
@@ -2,13 +2,14 @@ import React, { JSX } from 'react';
 import classNames from 'classnames';
 import styles from './typography.module.scss';
 
-export type TypographyVariant = 'title-1' | 'title-2' | 'sub-title' | 'buttontext' | 'text';
+export type TypographyVariant = 'title-1' | 'title-2' | 'title-3' | 'sub-title' | 'buttontext' | 'text';
 
 export type TypographyColor = 'black' | 'blue' | 'white';
 
 const VARIANT_DEFAULT_TAG_MAP: Record<TypographyVariant, keyof JSX.IntrinsicElements> = {
   'title-1': 'h1',
   'title-2': 'h1',
+  'title-3': 'h2',
   'sub-title': 'h2',
   buttontext: 'p',
   text: 'p',
