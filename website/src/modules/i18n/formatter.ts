69c45bd2b0e1560b519e3dc0c96adf08c654dcef
Simon Stucki <simon_stucki@hotmail.com>
2025-09-30 20:43:04 +0200

    Add cent formatter

--- website/src/modules/i18n/formatter.ts
+++ website/src/modules/i18n/formatter.ts
@@ -3,3 +3,7 @@ const numberFormatter = new Intl.NumberFormat('de-CH');
 export function formatNumber(value: number): string {
   return numberFormatter.format(value);
 }
+
+export function formatRappen(value: number): string {
+  return value.toFixed(2);
+}
