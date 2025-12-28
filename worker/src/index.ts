9fe4e4f5525accf3e7899f3bd7c7d59b65c5732b
Simon Stucki <simon_stucki@hotmail.com>
2025-12-28 15:58:28 +0100

    Add private endpoint setup

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,7 +1,17 @@
 import { Hono } from 'hono';
+import { env } from 'hono/adapter';
+import { basicAuth } from 'hono/basic-auth';
 import slotsList from './endpoints/slotsList';
 
 const app = new Hono<{ Bindings: Env }>();
+
+app.use('/api/private/*', async (c, next) => {
+  const password = String(env(c).ADMIN_KEY ?? '');
+  return basicAuth({
+    username: 'admin',
+    password,
+  })(c, next);
+});
 
 slotsList(app);
 
