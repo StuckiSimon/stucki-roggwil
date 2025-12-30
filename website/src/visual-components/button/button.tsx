cc92a00e0adcb1b40b0b1bc78d52dd1806eebd75
Simon Stucki <simon_stucki@hotmail.com>
2025-12-30 19:50:17 +0100

    Add native button

--- website/src/visual-components/button/button.tsx
+++ website/src/visual-components/button/button.tsx
@@ -3,12 +3,37 @@ import NextLink from 'next/link';
 import classNames from 'classnames';
 import styles from './button.module.scss';
 
-type Props = ComponentProps<typeof NextLink> & {
+type Props = ComponentProps<'button'> & {
   secondary?: boolean;
   accent?: boolean;
 };
 
-export const ButtonLink: React.FC<Props> = ({ className, children, accent = false, secondary = false, ...rest }) => {
+export const Button: React.FC<Props> = ({ className, children, secondary = false, accent = false, ...rest }) => {
+  return (
+    <button
+      className={classNames(className, styles.root, {
+        [styles.secondary]: secondary,
+        [styles.accent]: accent,
+      })}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+};
+
+type ButtonLinkProps = ComponentProps<typeof NextLink> & {
+  secondary?: boolean;
+  accent?: boolean;
+};
+
+export const ButtonLink: React.FC<ButtonLinkProps> = ({
+  className,
+  children,
+  accent = false,
+  secondary = false,
+  ...rest
+}) => {
   return (
     <NextLink
       className={classNames(className, styles.root, {
