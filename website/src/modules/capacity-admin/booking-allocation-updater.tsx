3ff86632f40a7fd6cc352e14d387a3663dae11b1
Simon Stucki <simon_stucki@hotmail.com>
2025-12-30 19:51:06 +0100

    Add form submit section

--- website/src/modules/capacity-admin/booking-allocation-updater.tsx
+++ website/src/modules/capacity-admin/booking-allocation-updater.tsx
@@ -5,6 +5,9 @@ import isNil from '@/core/util/is-nil';
 import { BookingPreviewCell } from '@/modules/capacity-admin/booking-preview-cell';
 import { CalendarWeekList, WeekChildren } from '@/modules/capacity-admin/calendar-week-list';
 import { getWeeksForCapacityEntries } from '@/modules/capacity-admin/services/getWeeksForCapacityEntries';
+import { Button } from '@/visual-components/button/button';
+import { Typography } from '@/visual-components/typography/typography';
+import styles from './booking-allocation-updater.module.scss';
 
 type Props = {
   unfilteredEntries: CapacityEntry[] | null;
@@ -19,13 +22,19 @@ export const BookingAllocationUpdater: React.FC<Props> = ({ unfilteredEntries })
   }
 
   return (
-    <CalendarWeekList
-      weeks={weekCapacities.map(({ weekStart, days }) => ({
-        weekStart,
-        children: days.map((day) =>
-          isNil(day) ? null : <BookingPreviewCell hours={day.capacityHours} />,
-        ) as WeekChildren,
-      }))}
-    />
+    <div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <Typography className={styles.title}>✅ Die Daten sind valide und können aktualisiert werden.</Typography>
+        <Button type="submit">Buchungen aktualisieren</Button>
+      </form>
+      <CalendarWeekList
+        weeks={weekCapacities.map(({ weekStart, days }) => ({
+          weekStart,
+          children: days.map((day) =>
+            isNil(day) ? null : <BookingPreviewCell hours={day.capacityHours} />,
+          ) as WeekChildren,
+        }))}
+      />
+    </div>
   );
 };
