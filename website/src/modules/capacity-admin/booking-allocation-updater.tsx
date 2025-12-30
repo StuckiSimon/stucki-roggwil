024652e1ce57bce34dd9b08ec4d791825ad77ebe
Simon Stucki <simon_stucki@hotmail.com>
2025-12-30 20:12:59 +0100

    Integrate submit for bookings

--- website/src/modules/capacity-admin/booking-allocation-updater.tsx
+++ website/src/modules/capacity-admin/booking-allocation-updater.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { reduceBookedHourEntries } from '@/modules/capacity-admin/services/reduceBookedHourEntries';
 import { CapacityEntry } from '@/modules/capacity-admin/types';
-import isNil from '@/core/util/is-nil';
+import isNil, { notNil } from '@/core/util/is-nil';
 import { BookingPreviewCell } from '@/modules/capacity-admin/booking-preview-cell';
 import { CalendarWeekList, WeekChildren } from '@/modules/capacity-admin/calendar-week-list';
 import { getWeeksForCapacityEntries } from '@/modules/capacity-admin/services/getWeeksForCapacityEntries';
 import { Button } from '@/visual-components/button/button';
 import { Typography } from '@/visual-components/typography/typography';
+import { usePostBookings } from '@/modules/worker/use-post-bookings';
+import { InformationBox } from '@/visual-components/information-box/information-box';
 import styles from './booking-allocation-updater.module.scss';
 
 type Props = {
@@ -16,6 +18,17 @@ type Props = {
 export const BookingAllocationUpdater: React.FC<Props> = ({ unfilteredEntries }) => {
   const entries = isNil(unfilteredEntries) ? null : reduceBookedHourEntries(unfilteredEntries);
   const weekCapacities = getWeeksForCapacityEntries(entries ?? []);
+
+  const { trigger, error, data } = usePostBookings();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!entries) return;
+
+    const bookingEntries = entries.map((e) => ({ date: e.date, bookedHours: e.capacityHours }));
+
+    await trigger(bookingEntries);
+  };
 
   if (isNil(entries)) {
     return null;
@@ -27,6 +40,20 @@ export const BookingAllocationUpdater: React.FC<Props> = ({ unfilteredEntries })
         <Typography className={styles.title}>✅ Die Daten sind valide und können aktualisiert werden.</Typography>
         <Button type="submit">Buchungen aktualisieren</Button>
       </form>
+      {notNil(error) ? (
+        <InformationBox
+          variant="error"
+          title="Fehler beim aktualisieren der Buchungen"
+          description={error?.description ?? undefined}
+        />
+      ) : null}
+      {notNil(data) ? (
+        <InformationBox
+          variant="success"
+          title="Buchungen wurden erfolgreich aktualisiert"
+          description="Die Buchungen sind jetzt gespeichert."
+        />
+      ) : null}
       <CalendarWeekList
         weeks={weekCapacities.map(({ weekStart, days }) => ({
           weekStart,
