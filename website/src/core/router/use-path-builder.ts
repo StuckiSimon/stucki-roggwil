5b44ec843c0267731e9996d711d294ed3f200b4f
Simon Stucki <simon_stucki@hotmail.com>
2026-01-01 19:43:07 +0100

    Sort path builders alphabetically

--- website/src/core/router/use-path-builder.ts
+++ website/src/core/router/use-path-builder.ts
@@ -1,18 +1,18 @@
 export const usePathBuilder = () => {
   return {
     aboutPath: () => '/about',
+    adminBookingsPath: () => '/admin/bookings',
+    adminPath: () => '/admin',
+    allInclusiveLeasingPath: () => '/all-inclusive-leasing',
+    bookingPath: () => '/booking',
     contactPath: () => '/contact',
     homePath: () => '/',
-    servicesPath: () => '/services',
-    stockPath: () => '/stock',
-    springExhibitionPath: () => '/spring-exhibition',
-    allInclusiveLeasingPath: () => '/all-inclusive-leasing',
-    vehicleRentPath: () => '/vehicle-rent',
-    tireServicePath: () => '/tire-service',
-    reasonsForPath: () => '/reasons-for',
     mechJobPath: () => '/mech-job',
-    bookingPath: () => '/booking',
-    adminPath: () => '/admin',
-    adminBookingsPath: () => '/admin/bookings',
+    reasonsForPath: () => '/reasons-for',
+    servicesPath: () => '/services',
+    springExhibitionPath: () => '/spring-exhibition',
+    stockPath: () => '/stock',
+    tireServicePath: () => '/tire-service',
+    vehicleRentPath: () => '/vehicle-rent',
   };
 };
