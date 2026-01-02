8d1eb2d884435e62323a16a7b3f1d0f77cda4511
Simon Stucki <simon_stucki@hotmail.com>
2026-01-02 16:23:52 +0100

    Add reusable booking header

---
+++ website/src/modules/booking/core/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { GridItem } from '@/visual-components/grid/grid.tsx';
+import { Spacer } from '@/visual-components/spacer/spacer.tsx';
+import { Typography } from '@/visual-components/typography/typography.tsx';
+
+export const Header: React.FC = () => {
+  return (
+    <GridItem>
+      <Spacer size="07" />
+      <Typography variant="title-2" color="blue">
+        Online Termin buchen
+      </Typography>
+      <Spacer size="05" />
+    </GridItem>
+  );
+};
