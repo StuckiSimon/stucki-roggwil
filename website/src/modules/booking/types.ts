fe608ebf7c3d16d2bba611f11c6234bef38be38a
Simon Stucki <simon_stucki@hotmail.com>
2026-01-03 10:31:55 +0100

    Setup booking types

--- website/src/modules/booking/types.ts
+++ website/src/modules/booking/types.ts
@@ -1,6 +1,55 @@
+export enum BookingStep {
+  ServiceSelection = 'service-selection',
+  SlotSelection = 'slot-selection',
+  CustomerDetails = 'customer-details',
+}
+
 export enum ServiceType {
-  Service = 'service',
+  VehicleService = 'vehicle-service',
   TireChange = 'tire-change',
   VehicleCheck = 'vehicle-check',
   MotorVehicleInspection = 'motor-vehicle-inspection',
 }
+
+export enum VehicleServiceType {
+  SmallService = 'smallService',
+  LargeService = 'largeService',
+  Unknown = 'unknown',
+}
+
+export enum TireLocation {
+  Stored = 'stored',
+  BringYourOwn = 'bringYourOwn',
+}
+
+export enum TireType {
+  CompleteWheel = 'completeWheel',
+  TireOnly = 'tireOnly',
+}
+
+export enum TireCondition {
+  Good = 'good',
+  Unsafe = 'unsafe',
+  Uncertain = 'uncertain',
+}
+
+export enum TireAction {
+  CleanAndStore = 'cleanAndStore',
+  TakeWithYou = 'takeWithYou',
+  Dispose = 'dispose',
+}
+
+export type ServiceStorageData = {
+  initialSetDate: string;
+  vehicleService?: {
+    type: VehicleServiceType;
+  };
+  vehicleCheck?: boolean;
+  motorVehicleInspection?: boolean;
+  tireChange?: {
+    tireLocation: TireLocation | null;
+    tireType: TireType;
+    tireCondition: TireCondition;
+    tireAction: TireAction;
+  };
+};
