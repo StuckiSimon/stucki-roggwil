ffa22d5c26d87bbd40a2944272605d000d085993
Simon Stucki <simon_stucki@hotmail.com>
2026-01-03 10:35:18 +0100

    Create service storage helper hook

---
+++ website/src/modules/booking/service/use-service-storage-data.ts
@@ -0,0 +1,69 @@
+import useLocalStorage from '@/core/storage/use-local-storage.ts';
+import { StorageKey } from '@/core/storage/storage-key.ts';
+import { ServiceStorageData, ServiceType } from '@/modules/booking/types.ts';
+import { notNil } from '@/core/util/is-nil.ts';
+import { useEffect } from 'react';
+
+const SERVICE_TYPE_KEY_MAP: Record<ServiceType, keyof ServiceStorageData> = {
+  [ServiceType.VehicleService]: 'vehicleService',
+  [ServiceType.TireChange]: 'tireChange',
+  [ServiceType.VehicleCheck]: 'vehicleCheck',
+  [ServiceType.MotorVehicleInspection]: 'motorVehicleInspection',
+};
+
+// Maximum age of stored data: 7 days
+const MAX_AGE_MS = 1000 * 60 * 60 * 24 * 7;
+
+export function useServiceStorageData() {
+  const [serviceStorageData, setServiceStorageData] = useLocalStorage<ServiceStorageData>(StorageKey.BookingService, {
+    initialSetDate: new Date().toISOString(),
+  });
+
+  const initialSetDateString = serviceStorageData.initialSetDate;
+  useEffect(() => {
+    const isStorageDataOld = new Date().getTime() - new Date(initialSetDateString).getTime() > MAX_AGE_MS;
+    if (isStorageDataOld) {
+      setServiceStorageData({
+        initialSetDate: new Date().toISOString(),
+      });
+    }
+  }, [initialSetDateString, setServiceStorageData]);
+
+  function setServiceTypeData<T extends ServiceType>(
+    serviceType: T,
+    data: ServiceStorageData[(typeof SERVICE_TYPE_KEY_MAP)[T]] | undefined,
+  ) {
+    const key = SERVICE_TYPE_KEY_MAP[serviceType];
+    setServiceStorageData({
+      ...serviceStorageData,
+      [key]: data,
+    });
+  }
+
+  const hasBookableServiceConfigured =
+    notNil(serviceStorageData?.vehicleService) ||
+    notNil(serviceStorageData?.tireChange) ||
+    notNil(serviceStorageData?.vehicleCheck) ||
+    notNil(serviceStorageData?.motorVehicleInspection);
+
+  return {
+    hasBookableServiceConfigured,
+    setServiceTypeData,
+    hasServiceConfigured: (serviceType: ServiceType) => {
+      const key = SERVICE_TYPE_KEY_MAP[serviceType];
+      return notNil(serviceStorageData?.[key]);
+    },
+    setVehicleServiceData: (data: ServiceStorageData['vehicleService'] | undefined) => {
+      setServiceTypeData(ServiceType.VehicleService, data);
+    },
+    setTireChangeData: (data: ServiceStorageData['tireChange'] | undefined) => {
+      setServiceTypeData(ServiceType.TireChange, data);
+    },
+    setVehicleCheckData: (data: ServiceStorageData['vehicleCheck'] | undefined) => {
+      setServiceTypeData(ServiceType.VehicleCheck, data);
+    },
+    setMotorVehicleInspectionData: (data: ServiceStorageData['motorVehicleInspection'] | undefined) => {
+      setServiceTypeData(ServiceType.MotorVehicleInspection, data);
+    },
+  };
+}
