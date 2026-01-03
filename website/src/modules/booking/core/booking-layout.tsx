e3769126244e72fa3ca528a3388603dfb02eb039
Simon Stucki <simon_stucki@hotmail.com>
2026-01-03 10:32:10 +0100

    Add reusable booking layout component

---
+++ website/src/modules/booking/core/booking-layout.tsx
@@ -0,0 +1,21 @@
+import React, { ComponentProps } from 'react';
+import { Header } from '@/modules/booking/core/header.tsx';
+import { GridContainer, GridItem } from '@/visual-components/grid/grid.tsx';
+import { StepperNavigation } from '@/visual-components/stepper-navigation/stepper-navigation.tsx';
+
+type Props = {
+  children: React.ReactNode;
+  stepperConfig: ComponentProps<typeof StepperNavigation>['steps'];
+};
+
+export const BookingLayout: React.FC<Props> = ({ children, stepperConfig }) => {
+  return (
+    <GridContainer>
+      <Header />
+      <GridItem span="3">
+        <StepperNavigation steps={stepperConfig} />
+      </GridItem>
+      <GridItem span="6">{children}</GridItem>
+    </GridContainer>
+  );
+};
