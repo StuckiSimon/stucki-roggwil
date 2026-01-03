481f245042e1f49a6b53a823e6572b795f0ac02c
Simon Stucki <simon_stucki@hotmail.com>
2026-01-03 10:31:28 +0100

    Make description optional

--- website/src/visual-components/cta-picto-link/cta-picto-link.tsx
+++ website/src/visual-components/cta-picto-link/cta-picto-link.tsx
@@ -4,12 +4,13 @@ import Link from 'next/link';
 import { Pictogram } from '@/visual-components/icon/icon.tsx';
 import { Typography } from '@/visual-components/typography/typography.tsx';
 import styles from './cta-picto-link.module.scss';
+import { notNil } from '@/core/util/is-nil.ts';
 
 type Props = {
   href: string;
   picto: PictogramVariant;
   title: string;
-  description: string;
+  description?: string;
 };
 
 export const CtaPictoLink: React.FC<Props> = ({ href, picto, title, description }) => {
@@ -19,7 +20,7 @@ export const CtaPictoLink: React.FC<Props> = ({ href, picto, title, description 
       <Typography variant="title-3" color="blue" className={styles.title}>
         {title}
       </Typography>
-      <Typography className={styles.description}>{description}</Typography>
+      {notNil(description) ? <Typography className={styles.description}>{description}</Typography> : null}
     </Link>
   );
 };
