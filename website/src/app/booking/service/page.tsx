749c615f044d732c88bb3600378598404d7616b4
Simon Stucki <simon_stucki@hotmail.com>
2026-01-04 12:10:42 +0100

    Add service root page

---
+++ website/src/app/booking/service/page.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+import { Layout } from '@/modules/layout/layout';
+import { Index } from '@/modules/booking/service';
+
+export const metadata: Metadata = {
+  title: 'Dienstleistungswahl – Garage Stucki AG',
+  description:
+    'Definieren Sie Ihre Dienstleistung online. Schnell, einfach und zuverlässig – reservieren Sie jetzt Ihren Termin!',
+};
+
+export default async function Service() {
+  return (
+    <Layout>
+      <Index />
+    </Layout>
+  );
+}
