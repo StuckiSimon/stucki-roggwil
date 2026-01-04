2b4f083c024afec292304f7473b271a17f6f81ab
Simon Stucki <simon_stucki@hotmail.com>
2026-01-04 16:36:54 +0100

    Upgrade slot logic
    Add more slots for combined services.
    It's now possible to book multiple services which increases the duration.

--- worker/src/modules/booking/getSlotsForDuration.ts
+++ worker/src/modules/booking/getSlotsForDuration.ts
@@ -1,14 +1,18 @@
 import { parse, addHours, format } from 'date-fns';
 import { Slot } from './types';
 
-// Everything longer than FULL_DAY_HOURS is considered a full day booking
-// Currently all services except "service" are only one hour
-const FULL_DAY_HOURS = 1;
+// Everything longer than MINIMUM_DURATION_HOUR is considered a half-day booking
+const MINIMUM_DURATION_HOUR = 1;
+
+// Everything longer than HALF_DAY_HOURS is considered a full-day booking
+const HALF_DAY_HOURS = 3;
 
 const oneHourStartTimes = ['07:30', '11:00', '13:30', '16:30'];
 
+const halfDayStartTimes = ['07:30', '13:30'];
+
 export function getSlotsForDuration(day: string, duration: number): Slot[] {
-  if (duration > FULL_DAY_HOURS) {
+  if (duration > HALF_DAY_HOURS) {
     return [
       {
         date: day,
@@ -18,7 +22,12 @@ export function getSlotsForDuration(day: string, duration: number): Slot[] {
     ];
   }
 
-  return oneHourStartTimes.map((startHour) => {
+  let startHours = oneHourStartTimes;
+  if (duration > MINIMUM_DURATION_HOUR) {
+    startHours = halfDayStartTimes;
+  }
+
+  return startHours.map((startHour) => {
     const hour = parse(startHour, 'HH:mm', new Date());
     const endHour = format(addHours(hour, duration), 'HH:mm');
 
