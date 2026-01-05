4b0f194bcf35e5d6321ff3c37eb5fc1a7cfd988d
Simon Stucki <simon_stucki@hotmail.com>
2026-01-05 20:42:31 +0100

    Use service link builder

--- website/src/app/services/page.tsx
+++ website/src/app/services/page.tsx
@@ -5,6 +5,7 @@ import { PageHero } from '@/visual-components/page-hero/page-hero';
 import { Card, CardContainer } from '@/visual-components/card/card';
 import { GridContainer, GridItem } from '@/visual-components/grid/grid';
 import { usePathBuilder } from '@/core/router/use-path-builder';
+import { useServiceLinks } from '@/modules/services/use-service-links.ts';
 
 export const metadata: Metadata = {
   title: 'Dienstleistungen | Garage Stucki AG',
@@ -32,65 +33,9 @@ export default async function Services() {
     }[]
   >(SERVICES_QUERY);
 
-  const {
-    servicesPath,
-    stockPath,
-    allInclusiveLeasingPath,
-    tireServicePath,
-    vehicleRentPath,
-    reasonsForPath,
-    contactPath,
-  } = usePathBuilder();
+  const { servicesPath } = usePathBuilder();
 
-  function getLink(linkTarget: string): { href: string; text: string } {
-    switch (linkTarget) {
-      case 'contact':
-        return {
-          href: contactPath(),
-          text: 'Kontakt',
-        };
-      case 'stock':
-        return {
-          href: stockPath(),
-          text: 'Angebot',
-        };
-      case 'freizeitmobile':
-        return {
-          href: 'https://www.freizeitmobilestucki.ch',
-          text: 'freizeitmobilestucki.ch',
-        };
-      case 'kgm-models':
-        return {
-          href: 'https://stucki-roggwil.kgm.ch/brochures-and-pricelists/',
-          text: 'KGM Modelle',
-        };
-      case 'all-inclusive-leasing':
-        return {
-          href: allInclusiveLeasingPath(),
-          text: 'All-Inclusive Leasing',
-        };
-      case 'tire-service':
-        return {
-          href: tireServicePath(),
-          text: 'Reifenservice',
-        };
-      case 'rental':
-        return {
-          href: vehicleRentPath(),
-          text: 'Autovermietung',
-        };
-      case 'why-stucki':
-        return {
-          href: reasonsForPath(),
-          text: 'Wieso Stucki AG?',
-        };
-      default:
-        return {
-          href: contactPath(),
-          text: 'Kontakt',
-        };
-    }
-  }
+  const { getLink } = useServiceLinks();
 
   return (
     <Layout activePath={servicesPath()}>
