9fb6292d174331c9a1b2d17c21813356543f6e92
Simon Stucki <simon_stucki@hotmail.com>
2026-01-09 06:27:33 +0100

    Prevent filling up days

--- worker/src/modules/booking/isDayAvailableForHours.ts
+++ worker/src/modules/booking/isDayAvailableForHours.ts
@@ -1,9 +1,15 @@
 import { Capacity } from './types';
 
 const MIN_REQUIRED_HOURS_TO_CONSIDER_DAY_AVAILABLE = 4;
+const MIN_REMAINING_CAPACITY_HOURS_TO_CONSIDER_DAY_AVAILABLE = 3;
 
 export function isDayAvailableForHours(day: Capacity, desiredHours: number) {
   const availableHours = day.capacityHours - day.bookedHours;
+  const remainingHoursAfterBooking = availableHours - desiredHours;
 
-  return availableHours >= desiredHours && availableHours >= MIN_REQUIRED_HOURS_TO_CONSIDER_DAY_AVAILABLE;
+  if (remainingHoursAfterBooking < MIN_REMAINING_CAPACITY_HOURS_TO_CONSIDER_DAY_AVAILABLE) {
+    return false;
+  }
+
+  return availableHours >= MIN_REQUIRED_HOURS_TO_CONSIDER_DAY_AVAILABLE;
 }
