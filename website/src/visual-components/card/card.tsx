5823bf6998c8da54ee9ecce7d173b6a1616e3be6
Simon Stucki <simon_stucki@hotmail.com>
2026-03-03 21:04:57 +0100

    Redesign card component
    Make everything clickable.
    Update design to use shorter texts.

--- website/src/visual-components/card/card.tsx
+++ website/src/visual-components/card/card.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import NextLink from 'next/link';
 import { Typography } from '@/visual-components/typography/typography';
-import { ButtonLink } from '@/visual-components/button/button';
+import { Icon } from '@/visual-components/icon/icon';
 import styles from './card.module.scss';
 import { notNil } from '@/core/util/is-nil';
 
@@ -17,24 +18,22 @@ type Props = {
 export const Card: React.FC<Props> = ({ title, image, text, cta }) => {
   const hasImage = notNil(image);
   return (
-    <div className={styles.root}>
-      <div>
-        {hasImage ? (
-          <picture>
-            <img className={styles.image} src={`${image}?w=600&h=450&fit=crop`} alt={title} />
-          </picture>
-        ) : null}
-        <div className={styles.wrapper}>
-          <Typography variant="sub-title" color="blue" className={styles.title}>
-            {title}
-          </Typography>
-          <Typography>{text}</Typography>
-        </div>
+    <NextLink href={cta.href} className={styles.root} tabIndex={0} aria-label={title}>
+      {hasImage ? (
+        <picture className={styles.picture}>
+          <img className={styles.image} src={`${image}?w=600&h=450&fit=crop`} alt={title} />
+        </picture>
+      ) : null}
+      <div className={styles.wrapper}>
+        <Typography variant="buttontext" className={styles.title}>
+          {title}
+        </Typography>
+        <Typography className={styles.text}>{text}</Typography>
       </div>
-      <div className={styles.footer}>
-        <ButtonLink href={cta.href}>{cta.text}</ButtonLink>
-      </div>
-    </div>
+      <span className={styles.chevron} aria-hidden="true">
+        <Icon variant="chevron-right" size="32" />
+      </span>
+    </NextLink>
   );
 };
 
