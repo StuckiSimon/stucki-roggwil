9eb1bc104e1fde47fd51cd37103b23f9e46f5cac
Simon Stucki <simon_stucki@hotmail.com>
2026-05-23 13:54:12 +0200

    Prepare service for summer season

--- website/src/modules/booking/service/config.ts
+++ website/src/modules/booking/service/config.ts
@@ -11,29 +11,29 @@ export type ServiceTypeKeyMap = typeof SERVICE_TYPE_KEY_MAP;
 
 export const VEHICLE_CHECK_PACKAGE_OPTIONS = [
   {
-    label: 'Frühlingskontrolle',
+    label: 'Feriencheck',
     value: VehicleCheckPackage.FK,
     subLabel: 'Technische Kontrolle aller wichtigen Funktionen und Niveaus',
     price: 'Fr. 59.–',
   },
-  {
+  /*{
     label: 'Unterbodenwäsche',
     value: VehicleCheckPackage.CHRE,
     subLabel: 'Unterboden abdampfen, Fahrzeug waschen',
     price: 'Fr. 69.–',
-  },
+  },*/
   {
-    label: 'Frühlingskontrolle inkl. Waschen',
+    label: 'Feriencheck inkl. Waschen',
     value: VehicleCheckPackage.FKWASCH,
-    subLabel: 'Frühlingskontrolle und Fahrzeug waschen',
+    subLabel: 'Feriencheck und Fahrzeug waschen',
     price: 'Fr. 74.–',
   },
-  {
-    label: 'Frühlingskontrolle inkl. Unterbodenwäsche',
+  /*{
+    label: 'Feriencheck inkl. Unterbodenwäsche',
     value: VehicleCheckPackage.FKCHRE,
-    subLabel: 'Frühlingskontrolle inkl. Unterbodenwäsche und Fahrzeug waschen',
+    subLabel: 'Feriencheck inkl. Unterbodenwäsche und Fahrzeug waschen',
     price: 'Fr. 128.–',
-  },
+  },*/
   {
     label: 'Frühlingsputz Light',
     value: VehicleCheckPackage.FPLIGHT,
@@ -43,21 +43,20 @@ export const VEHICLE_CHECK_PACKAGE_OPTIONS = [
   {
     label: 'Frühlingsputz Standard',
     value: VehicleCheckPackage.FPSTANDARD,
-    subLabel: 'Frühlingskontrolle, Innenreinigung, Fahrzeug waschen, Türfälze reinigen',
+    subLabel: 'Feriencheck, Innenreinigung, Fahrzeug waschen, Türfälze reinigen',
     price: 'Fr. 119.–',
   },
   {
     label: 'Frühlingsputz Spezial',
     value: VehicleCheckPackage.FPSPEZ,
-    subLabel:
-      'Frühlingskontrolle, Innenreinigung, Fahrzeug waschen, Türfälze reinigen, Unterboden und Motorraum abdampfen',
+    subLabel: 'Feriencheck, Innenreinigung, Fahrzeug waschen, Türfälze reinigen, Unterboden und Motorraum abdampfen',
     price: 'Fr. 199.–',
   },
   {
     label: 'Frühlingsputz Luxus',
     value: VehicleCheckPackage.FPLUX,
     subLabel:
-      'Frühlingskontrolle, Innenreinigung, Fahrzeug waschen, Türfälze reinigen, Unterboden und Motorraum abdampfen, Polieren und Einwachsen des Lackes',
+      'Feriencheck, Innenreinigung, Fahrzeug waschen, Türfälze reinigen, Unterboden und Motorraum abdampfen, Polieren und Einwachsen des Lackes',
     price: 'ab Fr. 299.–',
   },
 ];
